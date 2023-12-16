6abe9b6a8e420eda5a054a2e576d8e250018bf2e
DanielLinker <dima.linki@web.de>
2023-12-16 23:23:27 +0100

    Füge maximalwert_in_einer_liste.js

--- kolega/maximalwert_in_einer_liste.js
+++ kolega/maximalwert_in_einer_liste.js
@@ -1,4 +1,8 @@
-
+//Aufgabe: "Maximalwert in einer Liste"
+// Beschreibung: Schreibe eine Funktion in deiner bevorzugten Programmiersprache, die ein Array von Zahlen als Parameter annimmt und die größte Zahl in diesem Array zurückgibt.
+//
+// Beispiel:
+// Bei einem Array [3, 7, 2, 5, 6] sollte die Funktion 7 zurückgeben, da dies die größte Zahl im Array ist.
 let max = [3, 7, 2, 5, 6]
 
 const maxValue = Math.max(...max)
