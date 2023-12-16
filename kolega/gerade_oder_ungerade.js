96ccd938e9dbbab922adeade3096253f8cdc0685
DanielLinker <dima.linki@web.de>
2023-12-16 23:08:08 +0100

    Füge gerade_oder_ungerade Funktion hinzu

---
+++ kolega/gerade_oder_ungerade.js
@@ -0,0 +1,11 @@
+const gerade_oder_ungerade =  ((t) => {
+    if (t % 2 === 0) {
+        return 'gerade'
+    }else {
+        return 'ungerade'
+    }
+})
+
+
+gerade_oder_ungerade(5)//?
+gerade_oder_ungerade(4)//?
