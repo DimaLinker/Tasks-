b75669114c7f6846b165d9881787ab3e29215563
Dima Linker <daniel.linker@mailbox.org>
2023-12-18 12:07:45 +0100

    Mindestwert_in_einem_Array_finden

---
+++ kolega/mindestwert_in_einem_Array_finden.js
@@ -0,0 +1,8 @@
+//Aufgabe: "Mindestwert in einem Array finden"
+// Beschreibung: Schreibe eine Funktion in deiner bevorzugten Programmiersprache, die ein Array von Zahlen entgegennimmt und die kleinste Zahl in diesem Array zurückgibt.
+//
+// Beispiel:
+// Gegeben sei das Array [34, -345, -1, 100]. Die Funktion sollte -345 zurückgeben, da dies die kleinste Zahl im Array ist.
+const minNumber = [34, -345, -1, 100];
+const outputNum = Math.min(...minNumber)//?
+console.log(outputNum)
