415d274f3cd8a8efda4d78dbf9faf6a35a24a4dd
Dima Linker <daniel.linker@mailbox.org>
2023-12-18 20:45:54 +0100

    SQL_Aufgaben_Start

---
+++ sql_aufgaben/auswahl_und_filterung.sql
@@ -0,0 +1,19 @@
+--Wählen Sie alle Nutzer aus, die den Status 'Student' haben.
+
+--Finden Sie alle Kurse, die eine Bewertung von mehr als 4.5 haben.
+
+--Listen Sie alle Nutzer auf, deren Vorname mit 'L' beginnt.
+
+--Zeigen Sie alle Kommentare an, die eine Bewertung von weniger als 3 erhalten haben.
+
+--Ermitteln Sie alle Bestellungen, bei denen der Bezahlstatus 'TRUE' ist.
+
+--Wählen Sie alle Kurse aus, deren Preis unter 50 Euro liegt.
+
+--Finden Sie alle Nutzer, die in 'London' wohnen.
+
+--Listen Sie alle Kurse auf, die in der Kategorie 'Programmierung' angeboten werden.
+
+--Zeigen Sie alle Kurse an, die auf Englisch angeboten werden und deren Teilnehmerzahl über 500 liegt.
+
+--Wählen Sie alle Gutscheine aus, die einen Rabatt von 50% oder mehr bieten.
